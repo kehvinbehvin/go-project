2b925ace7f54c196b77901daca4eae04856c1d71
kevin <hokevin78@gmail.com>
2022-12-29 23:50:28 +0800

    feat: add zap logger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,8 +4,46 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"go.uber.org/zap"
 )
 
 func main() {
-	fmt.Println("🤓")
+	os.Exit(start())
+}
+
+func start() int {
+	logEnv := getStringOrDefault("LOG_ENV", "development")
+	log, err := createLogger(logEnv)
+	if err != nil {
+		fmt.Println("Error setting up the logger:", err)
+		return 1
+	}
+	defer func() {
+		// If we cannot sync, there's probably something wrong with outputting logs,
+		// so we probably cannot write using fmt.Println either. So just ignore the error.
+		_ = log.Sync()
+	}()
+
+	return 0
+}
+
+func createLogger(env string) (*zap.Logger, error) {
+	switch env {
+	case "production":
+		return zap.NewProduction()
+	case "development":
+		return zap.NewDevelopment()
+	default:
+		return zap.NewNop(), nil
+	}
+}
+
+func getStringOrDefault(name, defaultV string) string {
+	v, ok := os.LookupEnv(name)
+	if !ok {
+		return defaultV
+	}
+	return v
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,7 @@ go 1.18
 require (
 	github.com/go-chi/chi/v5 v5.0.8 // indirect
 	github.com/matryer/is v1.4.0 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
 )
